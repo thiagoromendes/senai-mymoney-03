478b9734985b5b5c402fa06869e645dca6290cc8
Thiago Mendes <thiago.mendes@evoluum.com.br>
2023-08-27 12:19:23 -0300

    chore: fisrt commit in repository

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:my_money/src/modules/initial/page/initial_page.dart';
+import 'package:my_money/src/modules/login/page/login_page.dart';
+import 'package:my_money/src/modules/personal_register/page/personal_register_page.dart';
+import 'package:my_money/src/modules/register/page/register_page.dart';
+import 'package:my_money/src/router/app_router.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Money',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
+      ),
+      home: const InitialPage(),
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      routes: {
+        AppRouter.login: (context) => const LoginPage(),
+        AppRouter.register: (context) => const RegisterPage(),
+        AppRouter.personalRegister: (context) => const PersonalRegisterPage(),
+      },
+    );
+  }
+}
